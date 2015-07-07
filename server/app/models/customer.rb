98c266e75d39170006e89db185e34de6f2598047
Tobia Cassandro <tobiacassandro@gmail.com>
2015-07-07 16:03:14 +0100

    server up and running properly again

--- server/app/models/customer.rb
+++ server/app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ActiveRecord::Base
+  # if we delete a customer object we delete all orders linked to that customer ( we do not want to leaave orphan rder objects)
+   has_many :orders, dependent: :destroy
+   has_one :profile
 end
