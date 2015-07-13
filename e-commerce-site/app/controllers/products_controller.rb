47812d64e0bdacd5223b53cfe10035bfaf2843c1
Tobia Cassandro <tobiacassandro@gmail.com>
2015-07-13 15:47:42 +0100

    added orderproduct view create.js.erb

--- e-commerce-site/app/controllers/products_controller.rb
+++ e-commerce-site/app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    @order_item = current_order.order_items.new
+    @order_product = current_order.order_products.new
   end
 
   def create
