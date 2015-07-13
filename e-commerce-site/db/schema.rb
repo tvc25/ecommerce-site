16ad237c3fd7c58916eae6e6d02a2d33ce5b4eeb
Tobia Cassandro <tobiacassandro@gmail.com>
2015-07-13 13:54:10 +0100

    added columns to order model and added OrderStatus model

--- e-commerce-site/db/schema.rb
+++ e-commerce-site/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713122841) do
+ActiveRecord::Schema.define(version: 20150713125304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20150713122841) do
   create_table "order_products", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
